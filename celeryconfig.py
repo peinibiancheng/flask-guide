82a9d32b4652697962a87dab53f803d5d47c9e84
peinibiancheng <peinibiancheng@hotmail.com>
2022-08-24 06:29:04 +0800

    celery

---
+++ celeryconfig.py
@@ -0,0 +1,8 @@
+broker_url = 'redis://localhost'
+# result_backend = 'redis://localhost'
+result_backend = 'db+mysql://ysrd:sunwenquan@localhost/ysrd'
+task_erializer = 'json'
+result_serializer = 'json'
+accept_content = ['json']
+timezone = 'Asia/Shanghai'
+enable_utc = True
